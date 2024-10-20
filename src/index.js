4a44db552598e68307feab18b7c76c9f6b485868
Guilherme Mattoso <85652034+mattosoguilherme@users.noreply.github.com>
2024-10-20 03:57:51 -0300

    funcionando

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3010 || process.env.PORT;
+const port = 3000 || process.env.PORT;
 const bodyParser = require('body-parser');
 const Routers = require("../src/routes/message.routes")
+const YAML = require('yamljs');
+const swagger = require('swagger-ui-express');
+const path = require('path');
+const { log } = require('console');
+const teste = path.join(__dirname, '../src/swagger.yaml');
+log(teste)
+const swaggerDocument = YAML.load(teste);
+
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/zapbo", Routers);
+app.use("/", Routers);
+app.use("/api", swagger.serve, swagger.setup(swaggerDocument))
 
 
 app.listen(port, () => {
